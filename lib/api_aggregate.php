76e93eaa8d6cc1c1768311918ca65e4cdd820ba9
Josh Soref <jsoref@users.noreply.github.com>
2022-05-31 11:47:31 -0400

    spelling: dynamically
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- lib/api_aggregate.php
+++ lib/api_aggregate.php
@@ -1938,7 +1938,7 @@ function draw_aggregate_template_graph_config($aggregate_template_id, $graph_tem
 
 	html_end_box(false, true);
 
-	/* some javascript do dinamically disable non-overriden fields */
+	/* some javascript do dynamically disable non-overriden fields */
 	?>
 	<script type='text/javascript'>
 
